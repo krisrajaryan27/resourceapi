355d24ddf3536313fc366c51704081e78d254174
kvatheer <kverma@atheerair.com>
2020-09-12 22:39:34 +0530

    Reverting redundant code

--- src/main/java/com/company/codeserver/entities/Project.java
+++ src/main/java/com/company/codeserver/entities/Project.java
@@ -1,16 +1,20 @@
 package com.company.codeserver.entities;
 
 import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
+import java.time.Instant;
 
 @Data
 @Entity
 @Table(name = "project")
-public class Project extends BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public class Project {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -27,5 +31,14 @@ public class Project extends BaseEntity {
     @JoinColumn(name = "sdlc_system_id")
     @NotNull
     private SdlcSystem sdlcSystem;
+
+    @Column(name = "created_date", nullable = false)
+    @CreatedDate
+    private Instant createdDate;
+
+    @Column(name = "last_modified_date", nullable = false)
+    @LastModifiedDate
+    private Instant lastModifiedDate;
+
 
 }
--- src/main/java/com/company/codeserver/entities/SdlcSystem.java
+++ src/main/java/com/company/codeserver/entities/SdlcSystem.java
@@ -2,14 +2,19 @@ package com.company.codeserver.entities;
 
 import lombok.Data;
 import org.hibernate.validator.constraints.URL;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
 import javax.validation.constraints.NotEmpty;
+import java.time.Instant;
 
 @Data
 @Entity
 @Table(name = "sdlc_system")
-public class SdlcSystem extends BaseEntity {
+@EntityListeners(AuditingEntityListener.class)
+public class SdlcSystem {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -22,5 +27,13 @@ public class SdlcSystem extends BaseEntity {
 
     @Column(name = "description")
     private String description;
+
+    @Column(name = "created_date", nullable = false)
+    @CreatedDate
+    private Instant createdDate;
+
+    @Column(name = "last_modified_date", nullable = false)
+    @LastModifiedDate
+    private Instant lastModifiedDate;
 
 }
